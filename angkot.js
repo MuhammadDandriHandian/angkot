78f165e9e2b8d61d6a6c5b0fc7f3b5a699c62595
dandri <mdhandian@gmail.com>
2023-12-31 19:26:24 +0700

    update js

--- angkot.js
+++ angkot.js
@@ -4,10 +4,27 @@
     //     angkot++;
     // }
 
-    // cara yg lebih benar
+    // cara yg lebih benar dengan while
+    // var angkot = 1;
+    // var jmlAngkot = 5;
+    // while(angkot <= jmlAngkot) {
+    //     console.log("Angkot No." + angkot + " beroperasi dengan baik.");
+    //     angkot++;
+    // }
+
+    // pake for
+    // for(var angkot=1; angkot <= 10; angkot++){
+    //     console.log("Angkot No. " + angkot + " siap beroperasi")
+    // }
+
+    // Studi kasus ada 4 angkot tidak bisa beroperasi (pake while dan for)
     var angkot = 1;
-    var jmlAngkot = 5;
-    while(angkot <= jmlAngkot) {
-        console.log("Angkot No." + angkot + " beroperasi dengan baik.");
+    var jmlAngkot = 10;
+    var beroperasi = 3;
+    while (angkot <= beroperasi) {
+        console.log("Angkot No. " + angkot + " siap beroperasi")
         angkot++;
+    }
+    for (angkot = beroperasi + 1; angkot <= jmlAngkot; angkot++){
+        console.log("Angkot No. " + angkot + " tidak dapat beroperasi")
     }
