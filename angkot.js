4006e805ecfa3c39f5105d342393fb91a945cde2
dandri <mdhandian@gmail.com>
2023-12-31 19:08:30 +0700

    memisahkan code js

---
+++ angkot.js
@@ -0,0 +1,13 @@
+ // var angkot = 1;
+    // while(angkot <= 10) {
+    //     console.log("Angkot No." + angkot + " beroperasi dengan baik.");
+    //     angkot++;
+    // }
+
+    // cara yg lebih benar
+    var angkot = 1;
+    var jmlAngkot = 5;
+    while(angkot <= jmlAngkot) {
+        console.log("Angkot No." + angkot + " beroperasi dengan baik.");
+        angkot++;
+    }
