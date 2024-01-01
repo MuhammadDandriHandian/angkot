b2e4b1604e6d5114ed0418a4755b3faa7c969cb3
dandri <mdhandian@gmail.com>
2024-01-01 13:39:41 +0700

    update angkot latihan while dan for

--- angkot.js
+++ angkot.js
@@ -1,30 +1,10 @@
- // var angkot = 1;
-    // while(angkot <= 10) {
-    //     console.log("Angkot No." + angkot + " beroperasi dengan baik.");
-    //     angkot++;
-    // }
-
-    // cara yg lebih benar dengan while
-    // var angkot = 1;
-    // var jmlAngkot = 5;
-    // while(angkot <= jmlAngkot) {
-    //     console.log("Angkot No." + angkot + " beroperasi dengan baik.");
-    //     angkot++;
-    // }
-
-    // pake for
-    // for(var angkot=1; angkot <= 10; angkot++){
-    //     console.log("Angkot No. " + angkot + " siap beroperasi")
-    // }
-
-    // Studi kasus ada 4 angkot tidak bisa beroperasi (pake while dan for)
-    var angkot = 1;
-    var jmlAngkot = 10;
-    var beroperasi = 3;
-    while (angkot <= beroperasi) {
-        console.log("Angkot No. " + angkot + " siap beroperasi")
-        angkot++;
-    }
-    for (angkot = beroperasi + 1; angkot <= jmlAngkot; angkot++){
-        console.log("Angkot No. " + angkot + " tidak dapat beroperasi")
-    }
+var angkot = 1;
+var jmlAngkot = 10;
+var beroperasi = 4;
+while (angkot <= beroperasi){
+    console.log("Angkot no. " + angkot + " siap beroperasi")
+    angkot++;
+}
+for (angkot = beroperasi + 1; angkot <= jmlAngkot; angkot++){
+    console.log("Angkot no. " + angkot + " tidak beroperasi")
+}
