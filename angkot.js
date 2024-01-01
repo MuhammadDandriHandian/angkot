b9af87cf0770ad2fa613c5242c84761e2ecee764
dandri <mdhandian@gmail.com>
2024-01-01 14:10:37 +0700

    latihan menggunakan if else

--- angkot.js
+++ angkot.js
@@ -1,10 +1,23 @@
+// var angkot = 1;
+// var jmlAngkot = 10;
+// var beroperasi = 4;
+// while (angkot <= beroperasi){
+//     console.log("Angkot no. " + angkot + " siap beroperasi")
+//     angkot++;
+// }
+// for (angkot = beroperasi + 1; angkot <= jmlAngkot; angkot++){
+//     console.log("Angkot no. " + angkot + " tidak beroperasi")
+// }
+
+// mengunakan if else
 var angkot = 1;
 var jmlAngkot = 10;
-var beroperasi = 4;
-while (angkot <= beroperasi){
-    console.log("Angkot no. " + angkot + " siap beroperasi")
-    angkot++;
-}
-for (angkot = beroperasi + 1; angkot <= jmlAngkot; angkot++){
-    console.log("Angkot no. " + angkot + " tidak beroperasi")
+var beroperasi = 8;
+
+for (angkot; angkot <= jmlAngkot; angkot++){
+    if (angkot <= beroperasi) {
+        console.log("Angkot No. " +angkot+ " siap beroperasi")
+    } else {
+        console.log("Angkot No. " +angkot+ " tidak dapat beroperasi")
+    }
 }
